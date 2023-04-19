eff583209fed64e655fad6e77eb70e11d8880b33
Lloyd McKenzie <lloyd@lmckenzie.com>
2023-04-19 08:25:57 -0600

    Test for Grahame

--- _genmaps.bat
+++ _genmaps.bat
@@ -7,7 +7,7 @@ ECHO Downloading most recent publisher to %jarlocation% - it's ~150 MB, so this 
 
 rem CALL POWERSHELL -command if ('System.Net.WebClient' -as [type]) {(new-object System.Net.WebClient).DownloadFile(\"%dlurl%\",\"%jarlocation%\") } else { Invoke-WebRequest -Uri "%dlurl%" -Outfile "%jarlocation%" }
 
-java -Dfile.encoding=UTF-8 -jar input-cache\validator_cli.jar -ig input/map-source/SDOHCC-Hunger-Vital-Sign-Map.map -compile http://hl7.org/fhir/us/sdoh-clinicalcare/StructureMap/SDOHCC-StructureMapHungerVitalSign -version 4.0 -output input/resources/structuremap/SDOHCC-StructureMapHungerVitalSign.xml
-java -Dfile.encoding=UTF-8 -jar input-cache\validator_cli.jar -ig input/map-source/SDOHCC-PRAPARE-Map.map -compile http://hl7.org/fhir/us/sdoh-clinicalcare/StructureMap/SDOHCC-StructureMapPRAPARE -version 4.0 -output input/resources/structuremap/SDOHCC-StructureMapPRAPARE.xml
+java -Dfile.encoding=UTF-8 -jar input-cache\validator_cli.jar -ig input/map-source/SDOHCC-Hunger-Vital-Sign-Map.fml -compile http://hl7.org/fhir/us/sdoh-clinicalcare/StructureMap/SDOHCC-StructureMapHungerVitalSign -version 4.0 -output input/resources/structuremap/SDOHCC-StructureMapHungerVitalSign.xml
+java -Dfile.encoding=UTF-8 -jar input-cache\validator_cli.jar -ig input/map-source/SDOHCC-PRAPARE-Map.fml -compile http://hl7.org/fhir/us/sdoh-clinicalcare/StructureMap/SDOHCC-StructureMapPRAPARE -version 4.0 -output input/resources/structuremap/SDOHCC-StructureMapPRAPARE.xml
 
 pause
